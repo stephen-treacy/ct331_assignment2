361942c6fc2e4645c994e3b66c6cea158a9f79d3
stephen-treacy <stephenttreacy@gmail.com>
2017-10-26 11:16:28 +0100

    Q1 Completed

--- assignment_q1.rkt
+++ assignment_q1.rkt
@@ -1,1 +1,6 @@
 #lang racket
+(define cons1(cons 34 76))
+(define cons2(cons 34 (cons 76 (cons 11 empty))))
+(define cons3(cons 'random (cons 91 (cons (cons 34 (cons 76 (cons 11 empty))) empty))))
+(define list1(list 'random 91 (list 34 76 11)))
+(define append1(append '(random) '(91) '((34 76 11))))
